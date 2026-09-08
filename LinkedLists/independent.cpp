e3387c0da816f9833d389ab5424a5e074e67a221
Divine Isaiah <divindeveloper@gmail.com>
2026-09-08 06:50:06 +0100

    updated linkedlists

--- LinkedLists/independent.cpp
+++ LinkedLists/independent.cpp
@@ -22,9 +22,15 @@ int main ()
     Node* first = newNode(10);
     Node* second = newNode(20);
     Node* third = newNode(30); 
-    // i would prefer i to call rhe function all i have ti do is to tell it the data i want to add and it reates a number of node based on that and links them for instance, newNode(10,20,30,40) I dot know if this is exam ok but that is jus wht my head says should be neater
+    Node* fourth = newNode(40);
+    // i would prefer to call the function and all i have to
+    //  do is tell it the data i want to add and it creates a number of node 
+    // based on that and links them for instance, 
+    // newNode(10,20,30,40) I dont know if this is exam ok but that is just what my head
+    //  says should be neater
 
     cout << first->data << '\n';
     cout << second->data << '\n';
     cout << third->data << '\n';
+    cout << fourth->data << "\n";
 }
